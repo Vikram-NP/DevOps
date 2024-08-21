861fc1c0cae2c9fec06fb0cbfd7a2d62ede9aca2
N P <vikram.n.p@accenture.com>
2024-08-21 21:56:42 +0530

    Shell_Script

---
+++ Shell_Script001.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+echo Line1
+echo Line2 
+
+echo -e "Line3\nLine4"
+echo -e "DevOps\tWith\tAWS\tTraining"
+
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
